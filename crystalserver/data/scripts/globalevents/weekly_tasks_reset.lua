ff5eb4170171aaf1cbe4f4a338040e76721e5589
Fark Dev <farkpkg@gmail.com>
2026-02-18 14:48:33 -0300

    Harden weekly tasks edge cases and reset scheduling

--- crystalserver/data/scripts/globalevents/weekly_tasks_reset.lua
+++ crystalserver/data/scripts/globalevents/weekly_tasks_reset.lua
@@ -1,12 +1,9 @@
 local weeklyTasksReset = GlobalEvent("WeeklyTasksReset")
 
 function weeklyTasksReset.onTime(interval)
-    local dateTable = os.date("*t")
-    if dateTable.wday == 2 then -- monday
-        WeeklyTasks.resetAllForWeek()
-    end
+    WeeklyTasks.resetAllForWeek(false)
     return true
 end
 
-weeklyTasksReset:time(configManager.getString(configKeys.GLOBAL_SERVER_SAVE_TIME))
+weeklyTasksReset:time("00:00:00")
 weeklyTasksReset:register()
